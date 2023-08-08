09d6b81e5d114717d6177f799073a788472463cf
Admin <Admin@DESKTOP-4H5FPDV>
2023-08-08 20:29:35 +0530

    ok

---
+++ DDF-WorkPulse/src/main/java/RoughPage/RoughClass.java
@@ -0,0 +1,35 @@
+package RoughPage;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RoughClass {
+
+	public static  void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");  
+
+	     WebDriver driver=new ChromeDriver();  
+	        
+	     driver.navigate().to("https://www.google.com/");  
+	     
+	     Thread.sleep(5000);
+	    
+	     driver.findElement(By.xpath("//input[@name = 'q']")).sendKeys("rohan");
+	     Thread.sleep(5000);
+	      driver.findElement(By.xpath("(//input[@name='btnK'])[1]")).click();
+	      driver.close();
+		
+		
+		
+	}
+
+}
